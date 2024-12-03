4c82b7a3986bdc105b0bd6d110e6aa2cf95b3465
Vova <volodimir.komarniczky@gmail.com>
2024-12-03 18:34:43 +0000

    first component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,10 @@
-// src/components/App.jsx
-
-import { Alert } from "./Alert";
-
 const App = () => {
   return (
     <>
-      <Alert variant="info">
-        Would you like to browse our recommended products?
-      </Alert>
-      <Alert variant="error">
-        There was an error during your last transaction
-      </Alert>
-      <Alert variant="success">
-        Payment received, thank you for your purchase
-      </Alert>
-      <Alert variant="warning">
-        Please update your profile contact information
-      </Alert>
+      <h2>Hello react! blah bla blah</h2>
+      <button>
+        <Click></Click>
+      </button>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 // Імпорт стилів нормалізації
 import "modern-normalize";
 import App from "./components/App.jsx";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
